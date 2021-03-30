a96c775b6fe49fd27d173c707f62798f0d8b992f
Lukas Suter <lukas.suter@swegon.com>
2021-03-30 17:23:23 +0200

    Added dashboard, authProvider and dataProvider

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import jsonServerProvider from "ra-data-json-server";
 import React from "react";
 import { Admin, Resource } from "react-admin";
 import "./App.css";
+import authProvider from "./authProvider";
+import Dashboard from "./Lists/Dashboard";
 import { PostCreate } from "./Lists/PostCreate";
 import { PostEdit } from "./Lists/PostEdit";
 import { PostList } from "./Lists/PostList";
 import { UserList } from "./Lists/UserList";
-
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
     <Resource
       name="posts"
       list={PostList}
@@ -21,6 +26,7 @@ const App = () => (
       icon={PostIcon}
     />
     <Resource name="users" list={UserList} icon={UserIcon} />
+
     {/* <Resource name="comments" list={ListGuesser} />
     <Resource name="albums" list={ListGuesser} />
     <Resource name="photos" list={ListGuesser} />
--- src/Lists/PostList.tsx
+++ src/Lists/PostList.tsx
@@ -1,22 +1,38 @@
+import { useMediaQuery } from "@material-ui/core";
 import * as React from "react";
 import {
   Datagrid,
   EditButton,
   List,
   ReferenceField,
+  SimpleList,
   TextField,
 } from "react-admin";
-import { PostFilter } from "./PostFilter";
 
-export const PostList = (props: any) => (
-  <List filters={<PostFilter />} {...props}>
-    <Datagrid>
-      <TextField source="id" />
-      <ReferenceField source="userId" reference="users">
-        <TextField source="name" />
-      </ReferenceField>
-      <TextField source="title" />
-      <EditButton />
-    </Datagrid>
-  </List>
-);
+export const PostList = (props: any) => {
+  // @ts-ignore
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+  return (
+    <List {...props}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(record) => record.title}
+          secondaryText={(record) => `${record.views} views`}
+          tertiaryText={(record) =>
+            new Date(record.published_at).toLocaleDateString()
+          }
+        />
+      ) : (
+        <Datagrid>
+          <TextField source="id" />
+          <ReferenceField label="User" source="userId" reference="users">
+            <TextField source="name" />
+          </ReferenceField>
+          <TextField source="title" />
+          <TextField source="body" />
+          <EditButton />
+        </Datagrid>
+      )}
+    </List>
+  );
+};
